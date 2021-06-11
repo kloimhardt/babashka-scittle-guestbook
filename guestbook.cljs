b293e2395f7b9a685233edb2b383e446330e8fc7
kloimhardt <kloimhardt@kloimhardts-MacBook-Air.local>
2021-06-11 20:11:59 +0200

    works

---
+++ guestbook.cljs
@@ -0,0 +1,106 @@
+;---
+; Excerpted from "Web Development with Clojure, Third Edition",
+; published by The Pragmatic Bookshelf.
+; Copyrights apply to this code. It may not be used to create training material,
+; courses, books, articles, and the like. Contact us if you are in doubt.
+; We make no guarantees that this code is fit for any purpose.
+; Visit http://www.pragmaticprogrammer.com/titles/dswdcloj3 for more book information.
+;---
+(ns guestbook.core
+  (:require [reagent.core :as r]
+            [reagent.dom :as dom]
+            [ajax.core :refer [GET POST]]
+            [clojure.string :as string]
+            #_[cljs.pprint :refer [pprint]]))
+
+;
+(defn get-messages [messages]
+  (GET "/messages"
+       {:headers {"Accept" "application/transit+json"}
+        :handler #(reset! messages (:messages %))}))
+;
+
+;
+(defn message-list [messages]
+  (println messages)
+  [:ul.messages
+   (for [{:keys [timestamp message name]} @messages]
+     ^{:key timestamp}
+     [:li
+      [:time (.toLocaleString timestamp)]
+      [:p message]
+      [:p " - " name]])])
+;
+
+;
+(defn send-message! [fields errors messages]
+  (.log js/console "huxi")
+
+  (.log js/console @fields)
+
+  (.log js/console "huxi2")
+  (POST "/message"
+        {;; :format :json
+         :headers
+         {"Accept" "application/transit+json"
+          ;;"x-csrf-token" (.-value (.getElementById js/document "token"))
+          }
+         :params @fields
+         :handler (fn [_]
+                    (swap! messages conj (assoc @fields
+                                                :timestamp (js/Date.)))
+                    (reset! fields nil)
+                    (reset! errors nil))
+         :error-handler (fn [e]
+                          (.log js/console (str e))
+                          (reset! errors (-> e :response :errors)))}))
+;
+
+;
+(defn errors-component [errors id]
+  (when-let [error (id @errors)]
+    [:div.notification.is-danger (string/join error)]))
+;
+
+;
+(defn message-form [messages]
+  (let [fields (r/atom {})
+        errors (r/atom nil)]
+    (fn []
+      [:div
+       [errors-component errors :server-error]
+       [:div.field
+        [:label.label {:for :name} "Name"]
+        [errors-component errors :name]
+        [:input.input
+         {:type :text
+          :name :name
+          :on-change #(swap! fields 
+                             assoc :name (-> % .-target .-value))
+          :value (:name @fields)}]]
+       [:div.field
+        [:label.label {:for :message} "Message"]
+        [errors-component errors :message]
+        [:textarea.textarea
+         {:name :message
+          :value (:message @fields)
+          :on-change #(swap! fields 
+                             assoc :message (-> % .-target .-value))}]]
+       [:input.button.is-primary
+        {:type :submit
+         :on-click #(send-message! fields errors messages)
+         :value "comment"}]])))
+
+(defn home []
+  (let [messages (r/atom nil)]
+    (get-messages messages)
+    (fn []
+      [:div.content>div.columns.is-centered>div.column.is-two-thirds
+       [:div.columns>div.column
+        [:h3 "Messages"]
+        [message-list messages]]
+       [:div.columns>div.column
+        [message-form messages]]])))
+;
+
+(dom/render [home] (.getElementById js/document "content"))
---
+++ guestbook_1.clj
@@ -0,0 +1,93 @@
+(require '[clojure.edn :as edn]
+         '[clojure.java.browse :as browse]
+         '[clojure.java.io :as io]
+         '[cognitect.transit :as transit]
+         '[org.httpkit.server :as srv])
+
+(import 'java.time.format.DateTimeFormatter
+        'java.time.LocalDateTime
+        'java.io.ByteArrayOutputStream)
+
+(def port 8083)
+
+(def bb-web-js (slurp "bb_web.js"))
+
+(defn html [cljs-code]
+  (str "
+  <!DOCTYPE html>
+  <html>
+  <head>
+  <meta charset=\"UTF-8\">
+  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
+  <link rel=\"shortcut icon\" href=\"data:,\">
+  <link rel=\"icon\" href=\"data:,\">
+  <link rel=\"apple-touch-icon\" href=\"data:,\">
+  <link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bulma@0.9.0/css/bulma.min.css\">
+  <script>"
+       bb-web-js
+       "</script>
+  <title>Guestbook 1</title>
+  </head>
+  <body>
+  <div id=\"cljs-app\">"
+       cljs-code
+       "</div>
+  <script>bb_web.app.run(\"cljs-app\")</script>
+  </body>
+  </html>"))
+
+(defn date [] (LocalDateTime/now))
+
+(def formatter (DateTimeFormatter/ofPattern "yyyy-MM-dd HH:mm:ss"))
+
+(def filename "m.txt")
+
+(defn readfile []
+  (if (.exists (io/file filename))
+    (edn/read-string (slurp filename))
+    {:messages []}))
+
+(defn db-save-message! [params]
+  (->> formatter
+       (.format (date))
+       (assoc params :timestamp)
+       (update (readfile) :messages conj)
+       pr-str
+       (spit filename)))
+
+(defn db-get-messages []
+  (:messages (readfile)))
+
+(defn home-save-message! [req]
+  (let [params (transit/read (transit/reader (:body req) :json))]
+    (db-save-message! params)
+    "post success!"))
+
+(defn home-message-list [_]
+  (let [out (ByteArrayOutputStream. 4096)
+        writer (transit/writer out :json)]
+    (transit/write writer {:messages (vec (db-get-messages))})
+    (.toString out)))
+
+(def cmd-line-args *command-line-args*)
+
+(defn home-page [request] (html (slurp (first cmd-line-args))))
+
+(defn home-routes [{:keys [:request-method :uri] :as req}]
+  (case [request-method uri]
+    [:get "/"] {:body (home-page req)
+                :status 200}
+    [:get "/messages"] {:headers {"Content-type" "application/transit+json"}
+                        :body (home-message-list req)
+                        :status 200}
+    [:post "/message"] {:body (home-save-message! req)
+                        :status 200}))
+
+(defn core-http-server []
+  (srv/run-server home-routes {:port port}))
+
+(let [url (str "http://localhost:" port "/")]
+  (core-http-server)
+  (println "serving" url)
+  (browse/browse-url url)
+  @(promise))
